23a523d68c9ebc14683030515553e11e47662a3f
Alpha <aykwong93@gmail.com>
2018-07-03 23:21:13 -0400

    Update javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,6 +22,21 @@ var trainStats = {
 
 var trainSchedule = [];
 
+var datetime = "",
+        date = "";
+
+function update() {
+    date = moment()
+    datetime.html(date.format('dddd, MMMM Do YYYY, HH:mm:ss a'));
+};
+
+$(document).ready(function(){
+    datetime = $('#currentTime')
+    update();
+    setInterval(update, 1000);
+    setInterval(updateTime, 1000);
+});
+
 $("#submit").on("click", function addTrain(event) {
   event.preventDefault();
 
@@ -30,21 +45,9 @@ $("#submit").on("click", function addTrain(event) {
   trainStats.firstTrainTime = moment($("#time").val(), "LT").format("HH:mm A");
   trainStats.frequency = parseInt($("#frequency").val().trim());
 
-  //Calculation for nextArrivel and minutesAway
-  let converted = moment(trainStats.firstTrainTime, "HH:mm").subtract(1, "years");
-  console.log(converted);
+  database.ref("trainTimes").push(trainStats);
 
-  let timeDiff = moment().diff(moment(converted), "minutes");
-  console.log(timeDiff);
-
-  let remainder = timeDiff % trainStats.frequency;
-  console.log(remainder);
-
-  trainStats.minutesAway = trainStats.frequency - remainder;
-  console.log(trainStats.minutesAway);
-
-  trainStats.nextArrival = moment().add(trainStats.minutesAway, "minutes").format("HH:mm A");
-  console.log(trainStats.nextArrival);
+  trainCalculations();
 
   trainSchedule.push(trainStats);
 
@@ -56,4 +59,37 @@ $("#submit").on("click", function addTrain(event) {
   train.append(`<td>${trainStats.nextArrival}</td>`);
   train.append(`<td>${trainStats.minutesAway}</td>`);
   $(".table").append(train);
+
+  $("#name").val("");
+  $("#destination").val("");
+  $("#time").val("");
+  $("#frequency").val("");
 });
+
+
+
+function trainCalculations() {
+    //Calculation for nextArrivel and minutesAway
+    let converted = moment(trainStats.firstTrainTime, "HH:mm").subtract(1, "years");
+    console.log(converted);
+    let timeDiff = moment().diff(moment(converted), "minutes");
+    console.log(timeDiff);
+    let remainder = timeDiff % trainStats.frequency;
+    console.log(remainder);
+    trainStats.minutesAway = trainStats.frequency - remainder;
+    console.log(trainStats.minutesAway);
+    trainStats.nextArrival = moment().add(trainStats.minutesAway, "minutes").format("HH:mm A");
+    console.log(trainStats.nextArrival);
+}
+
+function updateTime() {
+  for(var i = 0; 0 < trainSchedule.length; i++) {
+    trainStats.firstTrainTime = trainSchedule[i].firstTrainTime;
+    trainStats.frequency = trainSchedule[i].frequency;
+    trainCalculations();
+    let trainRef = ref.child()
+
+  }
+
+  database.ref("trainTimes").on("value")
+}
